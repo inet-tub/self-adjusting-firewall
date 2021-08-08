187671aa419fd797b88ea2238a40885309fd089f
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2021-08-08 22:10:46 +0000

    setup

--- follow/setup.sh
+++ follow/setup.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/drjdaly/tuplemerge.git
-mv src/Tuplemerge/TupleMergeOnline.h.changed tuplemerge/TupleMerge/TupleMergeOnline.h
+#git clone https://github.com/drjdaly/tuplemerge.git
+#mv src/Tuplemerge/TupleMergeOnline.h.changed tuplemerge/TupleMerge/TupleMergeOnline.h
 
-cd tuplemerge
-make -j main
+#cd tuplemerge
+#make -j main
 
-cd ./../Default/
+cd ./Default/
 make -j
