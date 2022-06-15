8508584c5146aa4e3d404d569d137dd4f71714f4
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2022-06-16 01:39:19 +0530

    plots

--- simulator/scripts/plots-multi.py
+++ simulator/scripts/plots-multi.py
@@ -24,57 +24,81 @@ fig, ax = plt.subplots(1,1)
 
 dfMrf = df[df["ALG"]=="follow"]
 
-ax.plot(dfMrf["Threads"],1/dfMrf["AvgCostPerReq"],label="MRF",lw=2, marker='s',markersize=10,c="0",ls='-',)
-ax.plot(dfMrf["Threads"], [ 1/(list(dfMrf["AvgCostPerReq"])[0]/(i**2)) for i in dfMrf["Threads"]],label="quadratic",lw=2, ls='--', marker='^',markersize=10,c="0.6")
-ax.plot(dfMrf["Threads"], [ 1/(list(dfMrf["AvgCostPerReq"])[0]/(i)) for i in dfMrf["Threads"]],label="linear",lw=2, ls='dotted', marker='.',markersize=10,c="0.5")
-dfMrf = df[df["ALG"]=="efficuts"]
-# ax.plot(dfMrf["Threads"],1/dfMrf["AvgCostPerReq"],label="efficuts",lw=2, marker='x',markersize=10)
+ax.plot(dfMrf["Threads"],[list(dfMrf["AvgCostPerReq"])[0]/i for i in dfMrf["AvgCostPerReq"]],label="MRF",lw=2, marker='s',markersize=10,c="0",ls='-',)
+# ax.plot(dfMrf["Threads"], [ i**2 for i in dfMrf["Threads"]],label="quadratic",lw=2, ls='--', marker='^',markersize=10,c="0.6")
+ax.plot(dfMrf["Threads"], [ i for i in dfMrf["Threads"]],label="linear",lw=2, ls='--', marker='o',markersize=10,c="0.5")
 
-ax.set_yscale('log')
+# ax.set_yscale('log')
 ax.legend(framealpha=0.5)
 ax.xaxis.grid(True,ls='--')
 ax.yaxis.grid(True,ls='--')
 ax.set_xlabel("Threads")
-ax.set_xscale('log',basex=2)
-ax.set_xticks([1,2,4,8,16,32,64])
-ax.set_xticklabels([1,2,4,8,16,32,64])
+# ax.set_xscale('log',basex=2)
+# ax.set_xticks([1,2,4,8,16,32,64])
+# ax.set_xticklabels([1,2,4,8,16,32,64])
+# ax.set_xlim(0,32)
+# ax.set_ylim(0,1000)
 
-fig, ax = plt.subplots(1,1)
-
-dfMrf = df[df["ALG"]=="efficuts"]
-
-ax.plot(dfMrf["Threads"],dfMrf["AvgCostPerReq"])
-ax.plot(dfMrf["Threads"], [ 1/list(dfMrf["AvgCostPerReq"])[0]/(i**2) for i in dfMrf["Threads"]],label="quadratic")
-ax.plot(dfMrf["Threads"], [ 1/list(dfMrf["AvgCostPerReq"])[0]/(i) for i in dfMrf["Threads"]],label="linear")
-
-ax.set_yscale('log')
-ax.legend()
-ax.xaxis.grid(True,ls='--')
-ax.yaxis.grid(True,ls='--')
-
+#%%
 
 fig, ax = plt.subplots(1,1)
 
 dfMrf = df[df["ALG"]=="list"]
 
-ax.plot(dfMrf["Threads"],dfMrf["AvgCostPerReq"])
-ax.plot(dfMrf["Threads"], [ 1/list(dfMrf["AvgCostPerReq"])[0]/(i**2) for i in dfMrf["Threads"]],label="quadratic")
-ax.plot(dfMrf["Threads"], [ 1/list(dfMrf["AvgCostPerReq"])[0]/(i) for i in dfMrf["Threads"]],label="linear")
+ax.plot(dfMrf["Threads"],[list(dfMrf["AvgCostPerReq"])[0]/i for i in dfMrf["AvgCostPerReq"]],label="Static List",lw=2, marker='s',markersize=10,c="0",ls='-',)
+# ax.plot(dfMrf["Threads"], [ i**2 for i in dfMrf["Threads"]],label="quadratic",lw=2, ls='--', marker='^',markersize=10,c="0.6")
+ax.plot(dfMrf["Threads"], [ i for i in dfMrf["Threads"]],label="linear",lw=2, ls='--', marker='o',markersize=10,c="0.5")
 
-ax.set_yscale('log')
-ax.legend()
+# ax.set_yscale('log')
+ax.legend(framealpha=0.5)
 ax.xaxis.grid(True,ls='--')
 ax.yaxis.grid(True,ls='--')
+ax.set_xlabel("Threads")
+# ax.set_xscale('log',basex=2)
+# ax.set_xticks([1,2,4,8,16,32,64])
+# ax.set_xticklabels([1,2,4,8,16,32,64])
+# ax.set_xlim(0,32)
+# ax.set_ylim(0,1000)
+
+#%%
+
+fig, ax = plt.subplots(1,1)
+
+dfMrf = df[df["ALG"]=="efficuts"]
+
+ax.plot(dfMrf["Threads"],[list(dfMrf["AvgCostPerReq"])[0]/i for i in dfMrf["AvgCostPerReq"]],label="Efficuts (Static tree-based)",lw=2, marker='s',markersize=10,c="0",ls='-',)
+# ax.plot(dfMrf["Threads"], [ i**2 for i in dfMrf["Threads"]],label="quadratic",lw=2, ls='--', marker='^',markersize=10,c="0.6")
+ax.plot(dfMrf["Threads"], [ i for i in dfMrf["Threads"]],label="linear",lw=2, ls='--', marker='o',markersize=10,c="0.5")
+
+# ax.set_yscale('log')
+ax.legend(framealpha=0.5)
+ax.xaxis.grid(True,ls='--')
+ax.yaxis.grid(True,ls='--')
+ax.set_xlabel("Threads")
+# ax.set_xscale('log',basex=2)
+# ax.set_xticks([1,2,4,8,16,32,64])
+# ax.set_xticklabels([1,2,4,8,16,32,64])
+# ax.set_xlim(0,32)
+# ax.set_ylim(0,1000)
+
+#%%
+
 
 fig, ax = plt.subplots(1,1)
 
 dfMrf = df[df["ALG"]=="hypercuts"]
 
-ax.plot(dfMrf["Threads"],dfMrf["AvgCostPerReq"])
-ax.plot(dfMrf["Threads"], [ 1/list(dfMrf["AvgCostPerReq"])[0]/(i**2) for i in dfMrf["Threads"]],label="quadratic")
-ax.plot(dfMrf["Threads"], [ 1/list(dfMrf["AvgCostPerReq"])[0]/(i) for i in dfMrf["Threads"]],label="linear")
+ax.plot(dfMrf["Threads"],[list(dfMrf["AvgCostPerReq"])[0]/i for i in dfMrf["AvgCostPerReq"]],label="Hypercuts (Static tree-based)",lw=2, marker='s',markersize=10,c="0",ls='-',)
+# ax.plot(dfMrf["Threads"], [ i**2 for i in dfMrf["Threads"]],label="quadratic",lw=2, ls='--', marker='^',markersize=10,c="0.6")
+ax.plot(dfMrf["Threads"], [ i for i in dfMrf["Threads"]],label="linear",lw=2, ls='--', marker='o',markersize=10,c="0.5")
 
-ax.set_yscale('log')
-ax.legend()
+# ax.set_yscale('log')
+ax.legend(framealpha=0.5)
 ax.xaxis.grid(True,ls='--')
 ax.yaxis.grid(True,ls='--')
+ax.set_xlabel("Threads")
+# ax.set_xscale('log',basex=2)
+# ax.set_xticks([1,2,4,8,16,32,64])
+# ax.set_xticklabels([1,2,4,8,16,32,64])
+# ax.set_xlim(0,32)
+# ax.set_ylim(0,1000)
